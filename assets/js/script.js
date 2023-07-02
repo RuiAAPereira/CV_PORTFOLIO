1457b64e6e24ca1d0428d0d395cb04f1c501e89e
Rui Pereira <ruisantos021@gmail.com>
2023-07-02 14:38:46 +0100

    added further information

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,68 @@ hamburgerBtn.addEventListener("click", () => {
 		sessionStorage.setItem("sidebarState", "closed");
 	}
 });
+
+// observer
+const header = document.querySelector("header");
+const teste = document.querySelector(".teste");
+
+const options = {
+	rootmargin: "-100px 0px 0px 0px",
+};
+
+const observer = new IntersectionObserver(function (entries, observer) {
+	entries.forEach((entry) => {
+		if (!entry.isIntersecting) {
+			header.classList.add("nav-scrolled");
+		} else {
+			header.classList.remove("nav-scrolled");
+		}
+	});
+}, options);
+
+// observer.observe(teste);
+
+// Navbar
+const headerHeight = document.querySelector("header").offsetHeight;
+
+document.documentElement.style.setProperty("--header-height", `${headerHeight}px`);
+
+// add event listener to all nav-links
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		// close sidebar
+		navbar.classList.remove("opened");
+		hamburgerBtn.setAttribute("data-state", "closed");
+		hamburgerBtn.setAttribute("aria-expanded", "false");
+		//save state to session
+		sessionStorage.setItem("sidebarState", "closed");
+
+		navLinks.forEach((a) => {
+			a.classList.remove("active");
+		});
+
+		// add active class to clicked navlink
+		link.classList.add("active");
+	});
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function () {
+		/* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+		this.classList.toggle("active");
+
+		/* Toggle between hiding and showing the active panel */
+		var panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+		} else {
+			panel.style.display = "block";
+		}
+	});
+}
